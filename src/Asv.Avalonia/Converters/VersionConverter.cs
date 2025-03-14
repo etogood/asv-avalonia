cbd7613cbc98b5ecf26c0037c5c8a0fcb6ca37a8
asv-soft-u06 <egorov@asv.me>
2025-03-14 18:07:44 +0500

    feat(asv-avalonia): implement a version converter for HomePageView
    I create a VersionConverter that removes the '+' and the numbers coming at the end from the version strings
    
    Integrated VersionConverter into HomePageView.axaml bindings.
    
    Asana: https://app.asana.com/1/1200746044236722/project/1203851531040615/task/1209604202284291?focus=true

---
+++ src/Asv.Avalonia/Converters/VersionConverter.cs
@@ -0,0 +1,34 @@
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace Asv.Avalonia
+{
+    public class VersionConverter : IValueConverter
+    {
+        public object? Convert(
+            object? value,
+            Type targetType,
+            object? parameter,
+            CultureInfo culture
+        )
+        {
+            if (value is not string version)
+            {
+                return value;
+            }
+
+            var plusIndex = version.IndexOf('+');
+            return plusIndex >= 0 ? version[..plusIndex] : version;
+        }
+
+        public object? ConvertBack(
+            object? value,
+            Type targetType,
+            object? parameter,
+            CultureInfo culture
+        )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
