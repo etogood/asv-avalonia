7c67ccc6c71f27f492cab04d002b147ecf5ffa20
Eugene <u03@asv.me>
2025-04-09 17:35:23 +0300

    some fixes

--- src/Asv.Avalonia/Shell/Pages/LogViewer/LogViewerViewModel.cs
+++ src/Asv.Avalonia/Shell/Pages/LogViewer/LogViewerViewModel.cs
@@ -8,6 +8,7 @@ using R3;
 
 namespace Asv.Avalonia;
 
+
 [ExportPage(PageId)]
 public class LogViewerViewModel : PageViewModel<LogViewerViewModel>, IPage
 {
@@ -20,7 +21,6 @@ public class LogViewerViewModel : PageViewModel<LogViewerViewModel>, IPage
 
     private readonly HashSet<LogLevel> _logLevels;
     private readonly HashSet<string> _logCategories;
-    private readonly HashSet<string> _logThreadIds;
 
     private readonly List<LogItemViewModel> _logItems;
     private readonly ObservableList<LogItemViewModel> _writableLogItems;
@@ -37,7 +37,6 @@ public class LogViewerViewModel : PageViewModel<LogViewerViewModel>, IPage
 
         _logLevels = new HashSet<LogLevel>();
         _logCategories = new HashSet<string>();
-        _logThreadIds = new HashSet<string>();
 
         _logItems = new List<LogItemViewModel>();
         PageSize = new BindableReactiveProperty<int>(PageSizes[0]);
@@ -45,18 +44,10 @@ public class LogViewerViewModel : PageViewModel<LogViewerViewModel>, IPage
         TotalItemsCount = new BindableReactiveProperty<int>(_logItems.Count);
         TotalPagesCount = new BindableReactiveProperty<int>(TotalItemsCount.Value / PageSize.Value);
         TotalItemsCount.Subscribe(_ => TotalPagesCount.Value = _ / PageSize.Value);
-        SelectedLevels = new ObservableList<LogLevel>()
-            .CreateWritableView(x => x)
-            .ToWritableNotifyCollectionChanged();
-        SelectedCategories = new ObservableList<string>()
-            .CreateWritableView(x => x)
-            .ToWritableNotifyCollectionChanged();
-        SelectedThreadIds = new ObservableList<string>()
-            .CreateWritableView(x => x)
-            .ToWritableNotifyCollectionChanged();
+        SelectedLevels = new ObservableList<LogLevel>();
+        SelectedCategories = new ObservableList<string>();
         AvailableLevels.ForEach(level => SelectedLevels.Add(level));
         AvailableCategories.ForEach(cls => SelectedCategories.Add(cls));
-        AvailableThreadIds.ForEach(thread => SelectedThreadIds.Add(thread));
         SearchText = new BindableReactiveProperty<string>();
         CurrentPageIndex = new BindableReactiveProperty<int>();
         SearchText.Subscribe(_ => CurrentPageIndex.OnNext(1));
@@ -66,20 +57,19 @@ public class LogViewerViewModel : PageViewModel<LogViewerViewModel>, IPage
         SearchText.Subscribe(text =>
             ClearSearchTextCommand.ChangeCanExecute(!string.IsNullOrEmpty(text))
         );
-
-        SelectAllThreadIdsCommand = new ReactiveCommand(_ =>
-            AvailableThreadIds.ForEach(thread => SelectedThreadIds.Add(thread))
-        );
-        DeselectAllThreadIdsCommand = new ReactiveCommand(_ => SelectedThreadIds.Clear());
-
+        
         SelectAllCategoriesCommand = new ReactiveCommand(_ =>
-            AvailableCategories.ForEach(cls => SelectedCategories.Add(cls))
-        );
+        {
+            SelectedCategories.Clear();
+            AvailableCategories.ForEach(cls => SelectedCategories.Add(cls));
+        });
         DeselectAllCategoriesCommand = new ReactiveCommand(_ => SelectedCategories.Clear());
 
         SelectAllLevelsCommand = new ReactiveCommand(_ =>
-            AvailableLevels.ForEach(level => SelectedLevels.Add(level))
-        );
+        {
+            SelectedLevels.Clear();
+            AvailableLevels.ForEach(level => SelectedLevels.Add(level));
+        });
         DeselectAllLevelsCommand = new ReactiveCommand(_ => SelectedLevels.Clear());
 
         ClearLogsCommand = new ReactiveCommand(ClearLogs);
@@ -87,6 +77,8 @@ public class LogViewerViewModel : PageViewModel<LogViewerViewModel>, IPage
             .ForEach(x => _logItems.Add(new LogItemViewModel(x.Timestamp.Ticks, x)));
         _logItems.Select(_ => _.Level).ForEach(level => _logLevels.Add(level));
         _logItems.Select(_ => _.Category).ForEach(category => _logCategories.Add(category));
+        SelectedLevels.AddRange(AvailableLevels);
+        SelectedCategories.AddRange(AvailableCategories);
         TotalItemsCount.Value = _logItems.Count;
         _log.OnMessage.Subscribe(message =>
         {
@@ -98,15 +90,25 @@ public class LogViewerViewModel : PageViewModel<LogViewerViewModel>, IPage
         _writableLogItems = new ObservableList<LogItemViewModel>(_logItems);
         var logView = _writableLogItems.CreateWritableView(x => x);
         logView.DisposeRemovedViewItems();
-       
-         logView.AttachFilter(FilterItem);
+        logView.AttachFilter(FilterItem);
         _writableLogItems.SetRoutableParent(this, false);
         _writableLogItems.Sort(LogItemDescendingComparer.Instance);
+        ApplyFilters = new ReactiveCommand(_ => _writableLogItems.ForEach(model => FilterItem(model)));
+        ClearFilters = new ReactiveCommand(_ =>
+        {
+            SelectedLevels.Clear();
+            SelectedLevels.AddRange(AvailableLevels);
+            SelectedCategories.Clear();
+            SelectedCategories.AddRange(AvailableCategories);
+            _writableLogItems.ForEach(model => FilterItem(model));
+        });
+        
         LogItemsView = logView.ToWritableNotifyCollectionChanged();
         LogItemsView.SyncCollection(
             logView.Skip(PageSize.Value * CurrentPageIndex.Value).Take(PageSize.Value),
             a => LogItemsView.Remove(a), 
-            x => LogItemsView.Add(x), LogItemDescendingComparer.Instance);
+            x => LogItemsView.Add(x),
+            LogItemDescendingComparer.Instance);
         Observable
             .CombineLatest(PageSize, CurrentPageIndex)
             .Subscribe(tuple =>
@@ -141,7 +143,6 @@ public class LogViewerViewModel : PageViewModel<LogViewerViewModel>, IPage
     public static List<int> PageSizes => [25, 50, 100, 200];
     public IEnumerable<LogLevel> AvailableLevels => _logLevels;
     public IEnumerable<string> AvailableCategories => _logCategories;
-    public IEnumerable<string> AvailableThreadIds => _logThreadIds;
     public BindableReactiveProperty<int> TotalItemsCount { get; }
     public BindableReactiveProperty<int> FilteredItemsCount { get; }
     public BindableReactiveProperty<string> SearchText { get; }
@@ -149,11 +150,8 @@ public class LogViewerViewModel : PageViewModel<LogViewerViewModel>, IPage
     public BindableReactiveProperty<int> TotalPagesCount { get; }
     public BindableReactiveProperty<int> PageSize { get; }
     public NotifyCollectionChangedSynchronizedViewList<LogItemViewModel> LogItemsView { get; set; }
-    public NotifyCollectionChangedSynchronizedViewList<LogLevel> SelectedLevels { get; set; }
-    public NotifyCollectionChangedSynchronizedViewList<string> SelectedCategories { get; set; }
-    public NotifyCollectionChangedSynchronizedViewList<string> SelectedThreadIds { get; set; }
-    public ReactiveCommand SelectAllThreadIdsCommand { get; }
-    public ReactiveCommand DeselectAllThreadIdsCommand { get; }
+    public ObservableList<LogLevel> SelectedLevels { get; set; }
+    public ObservableList<string> SelectedCategories { get; set; }
     public ReactiveCommand SelectAllCategoriesCommand { get; }
     public ReactiveCommand DeselectAllCategoriesCommand { get; }
     public ReactiveCommand SelectAllLevelsCommand { get; }
@@ -162,6 +160,9 @@ public class LogViewerViewModel : PageViewModel<LogViewerViewModel>, IPage
     public ReactiveCommand MoveToNextPageCommand { get; }
     public ReactiveCommand ClearSearchTextCommand { get; }
     public ReactiveCommand ClearLogsCommand { get; }
+    
+    public ReactiveCommand ApplyFilters { get; }
+    public ReactiveCommand ClearFilters { get;  }
 
     private bool FilterItem(LogItemViewModel item)
     {
@@ -170,11 +171,11 @@ public class LogViewerViewModel : PageViewModel<LogViewerViewModel>, IPage
             || item.Message.Contains(SearchText.Value, StringComparison.CurrentCultureIgnoreCase);
         FilteredItemsCount.Value = TotalItemsCount.Value;
 
-        // var containsLevel = SelectedLevels.Contains(item.Level);
-        // var containsClass = SelectedCategories.Contains(item.Category);
-        return containsMessage;
+         var containsLevel = SelectedLevels.Contains(item.Level);
+         var containsClass = SelectedCategories.Contains(item.Category);
+        return containsMessage
 
-        // && containsLevel && containsClass;
+         && containsLevel && containsClass;
     }
 
     private void MoveToPreviousPage()
