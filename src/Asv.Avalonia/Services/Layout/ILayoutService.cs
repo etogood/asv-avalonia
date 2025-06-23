9c2e014e5b389b9e6d30c3fc2b2dd469a1eaa05a
Alexey <248997@gmail.com>
2025-06-23 16:02:46 +0500

    feat: implement ILayoutService and LayoutService for managing user layouts

---
+++ src/Asv.Avalonia/Services/Layout/ILayoutService.cs
@@ -0,0 +1,57 @@
+using Asv.Cfg;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.LogicalTree;
+
+namespace Asv.Avalonia;
+
+public interface ILayoutService
+{
+    TPocoType Get<TPocoType>(IRoutable source, Lazy<TPocoType> defaultValue)
+        where TPocoType : class, new();
+
+    void Set<TPocoType>(IRoutable source, TPocoType value)
+        where TPocoType : class, new();
+
+    TPocoType Get<TPocoType>(StyledElement source, Lazy<TPocoType> defaultValue)
+        where TPocoType : class, new()
+    {
+        var control = source;
+        while (control != null)
+        {
+            if (control.DataContext is IRoutable routable)
+            {
+                return this.Get<TPocoType>(routable, defaultValue);
+                break;
+            }
+
+            // Try to find IRoutable DataContext in logical parent
+            control = control.GetLogicalParent() as Control;
+        }
+
+        throw new InvalidOperationException(
+            "No IRoutable DataContext found in the logical tree of the provided StyledElement."
+        );
+    }
+
+    void Set<TPocoType>(StyledElement source, TPocoType value)
+        where TPocoType : class, new()
+    {
+        var control = source;
+        while (control != null)
+        {
+            if (control.DataContext is IRoutable routable)
+            {
+                Set(routable, value);
+                break;
+            }
+
+            // Try to find IRoutable DataContext in logical parent
+            control = control.GetLogicalParent() as Control;
+        }
+
+        throw new InvalidOperationException(
+            "No IRoutable DataContext found in the logical tree of the provided StyledElement."
+        );
+    }
+}
