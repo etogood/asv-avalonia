0143e660f963297e3b50e4a4d46e8e7f5af87d54
Alexey <248997@gmail.com>
2025-06-24 19:54:16 +0500

    fix: restore default theme; add FrequencyControl; remove shell status bar;

---
+++ src/Asv.Avalonia/Controls/FrequencyScreen/FrequencyControl.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls.Primitives;
+
+namespace Asv.Avalonia;
+
+public class FrequencyControl : TemplatedControl
+{
+    public static readonly StyledProperty<string?> ValueProperty = AvaloniaProperty.Register<
+        FrequencyControl,
+        string?
+    >(nameof(Value));
+
+    public string? Value
+    {
+        get => GetValue(ValueProperty);
+        set => SetValue(ValueProperty, value);
+    }
+}
